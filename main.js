8e84c76acbead8c469c6c86c383018fb09ac8552
Arjun8464 <71544080+Arjun8464@users.noreply.github.com>
2021-10-30 10:49:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+leftWristx = 0;
+rightWristx = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550 , 550);
@@ -16,5 +22,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX" + noseX + "noseY" + noseY);
+
+        leftWristx = results[0].pose.leftWrist.x;
+        rightWristx = results[0].pose.rightWrist.x;
+        difference = leftWristx - rightWristx;
     }
+}
+
+function draw(){
+    background('#000000');
+    fill('#FFFFFF');
+    stroke('#FF0000')
+    square(noseX, noseY, difference)
 }
